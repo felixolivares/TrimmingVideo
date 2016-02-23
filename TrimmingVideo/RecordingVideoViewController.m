91f172d2387768ab8ef082c514ff453df580de4d
Felix Olivares <felixoe@gmail.com>
2016-02-22 20:29:41 -0600

    first commit

---
+++ TrimmingVideo/RecordingVideoViewController.m
@@ -0,0 +1,37 @@
+//
+//  RecordingVideoViewController.m
+//  TrimmingVideo
+//
+//  Created by Jose Felix Olivares Estrada on 2/21/16.
+//  Copyright © 2016 Jose Felix Olivares Estrada. All rights reserved.
+//
+
+#import "RecordingVideoViewController.h"
+
+@interface RecordingVideoViewController ()
+
+@end
+
+@implementation RecordingVideoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ TrimmingVideo/VideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoViewController.h
+//  TrimmingVideo
+//
+//  Created by Jose Felix Olivares Estrada on 2/21/16.
+//  Copyright © 2016 Jose Felix Olivares Estrada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoViewController : UIViewController
+
+@end
